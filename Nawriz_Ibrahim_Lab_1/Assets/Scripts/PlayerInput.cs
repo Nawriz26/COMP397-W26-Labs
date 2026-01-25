ad090a1a1a223a9aa70fac4add46426a7ef8c723
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-01-24 23:18:57 -0500

    Added the 'Look Action' in PlayerInput.cs

--- Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
+++ Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
@@ -4,16 +4,17 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     private InputAction move;
+    private InputAction look;
     
     void Start()
     {
-        move = new InputSystem.actions.FindAction("Player/Move");
+        move = InputSystem.actions.FindAction("Player/Move");
+        look = InputSystem.actions.FindAction("Player/Look");
     }
 
     void Update()
     {
         Vector2 readMove = move.ReadValue<Vector2>();
-
-        Debug.Log("Player Move Input: " + readMove);
+        Vector2 readLook = look.ReadValue<Vector2>();
     }
 }
