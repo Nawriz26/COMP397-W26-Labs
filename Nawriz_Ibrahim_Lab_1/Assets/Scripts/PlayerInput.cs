0e597a7a65c1c50d7a5fc53c604e34a1202a90bb
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-01-25 01:06:43 -0500

    Add Rotation of the camera in PlayerInput.cs

--- Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
+++ Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
@@ -1,4 +1,5 @@
 using KBCore.Refs;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,9 +13,10 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float maxSpeed = 10.0f;
     [SerializeField] private float gravity = -30.0f;
     private Vector3 velocity;
-
-
+    [SerializeField] private float rotationSpeed = 4.0f;
+    [SerializeField] private float mouseSensY = 5.0f;
     [SerializeField, Self] private CharacterController controller;
+    [SerializeField, Child] private Camera cam;
 
     private void OnValidate()
     {
@@ -31,9 +33,19 @@ public class PlayerInput : MonoBehaviour
     {
         Vector2 readMove = move.ReadValue<Vector2>();
         Vector2 readLook = look.ReadValue<Vector2>();
+        // Movement of the player
         Vector3 movement = transform.right * readMove.x + transform.forward * readMove.y;
         controller.Move(movement * maxSpeed * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        // Rotation of the player
+        transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
+
+        // Rotation of the camera
+        mouseSensY = mouseSensY * readLook.y;
+        mouseSensY = Mathf.Clamp(mouseSensY, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSensY, 0, 0);
+
     }
 }
