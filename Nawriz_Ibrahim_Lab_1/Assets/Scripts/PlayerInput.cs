fbfa0ccaf9258aaf16d538f6483133e0a9d259bb
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-01-24 23:35:50 -0500

    Added a 'Character Controller' in PlayerInput.cs

--- Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
+++ Nawriz_Ibrahim_Lab_1/Assets/Scripts/PlayerInput.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class PlayerInput : MonoBehaviour
 {
     private InputAction move;
     private InputAction look;
-    
+    [SerializeField] private CharacterController controller;
+
     void Start()
     {
         move = InputSystem.actions.FindAction("Player/Move");
         look = InputSystem.actions.FindAction("Player/Look");
+        controller = GetComponent<CharacterController>();
+        //if (controller == null)
+        //{
+        //    controller = gameObject.AddComponent<CharacterController>();
+        //}
     }
 
     void Update()
