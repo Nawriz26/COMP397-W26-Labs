01ca05b82e037a6d5e36e31587f99ed69d711ab0
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-03-19 03:29:58 -0400

    Fixed few issues with the Buttons in Menu.cs

--- Nawriz_Ibrahim_Lab_1/Assets/Scripts/Menu.cs
+++ Nawriz_Ibrahim_Lab_1/Assets/Scripts/Menu.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Menu : PersistentSingleton<Menu>
+public class Menu : MonoBehaviour
 {
+    private const string GAME_SCENE_NAME = "SampleScene";
+    private const string SAVE_FILE_NAME = "Menu";
+
+    [SerializeField] private Button playBtn;
     [SerializeField] private Button saveBtn;
     [SerializeField] private Button loadBtn;
 
     private void Start()
     {
+        playBtn.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(GAME_SCENE_NAME);
+        });
+
         saveBtn.onClick.AddListener(() =>
         {
-            SaveLoadSystem.Instance.gameData.fileName = "Menu";
-            SaveLoadSystem.Instance.gameData.sceneName = "SampleScene";
+            SaveLoadSystem.Instance.gameData.fileName = SAVE_FILE_NAME;
+            SaveLoadSystem.Instance.gameData.sceneName = GAME_SCENE_NAME;
             SaveLoadSystem.Instance.SaveGame();
         });
-        loadBtn.onClick.AddListener(() => {
-            SaveLoadSystem.Instance.LoadGame("Menu");
+
+        loadBtn.onClick.AddListener(() =>
+        {
+            SaveLoadSystem.Instance.LoadGame(SAVE_FILE_NAME);
         });
     }
 }
